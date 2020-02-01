d226e17dc72e90a6028ae76408b1b7a60ceb0c61
Brad Tracy <bradktracy@gmail.com>
2020-02-01 11:33:58 -0500

    Updates

---
+++ precache-manifest.488f597d57cc48fa92ab56630412a523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b7b618339f62d52a3deb599d95c16e",
+    "url": "/freelancer/index.html"
+  },
+  {
+    "revision": "fbda2e610336406da54d",
+    "url": "/freelancer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6889af4b667dd9778a7e",
+    "url": "/freelancer/static/js/2.fe78b9c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/freelancer/static/js/2.fe78b9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbda2e610336406da54d",
+    "url": "/freelancer/static/js/main.bf4c9da3.chunk.js"
+  },
+  {
+    "revision": "8ba34629021e65b4b62d",
+    "url": "/freelancer/static/js/runtime-main.72a9bd8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freelancer/static/media/logo.5d5d9eef.svg"
+  }
+]);
